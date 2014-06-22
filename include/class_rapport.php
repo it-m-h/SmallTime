43692a5e6271c2e209f34332cd1eb97a6ce3d124
IT-Master GmbH <info@it-master.ch>
2014-06-22 19:24:43 +0200

    Version 0.861 ----------------------------------------------------------------------- - Rapport wurde nicht gespeichert wenn das Verzeichnis nicht existierte

--- include/class_rapport.php
+++ include/class_rapport.php
@@ -2,7 +2,7 @@
 /*******************************************************************************
 * Rapport
 /*******************************************************************************
-* Version 0.8
+* Version 0.861
 * Author:  IT-Master GmbH
 * www.it-master.ch / info@it-master.ch
 * Copyright (c) , IT-Master GmbH, All rights reserved
@@ -15,27 +15,31 @@ class time_rapport{
 	}
 	function insert_rapport($_ordnerpfad, $_timestamp){
 		$rapport = $_POST['rapport'];
-        $_file = "./Data/".$_ordnerpfad."/Rapport/".date("Y.m.d", $_timestamp);
-        $fp = fopen($_file,"w+");
-        fputs($fp, $rapport);
-        fclose($fp);
+		$_file = "./Data/".$_ordnerpfad."/Rapport/".date("Y.m.d", $_timestamp);
+		$_folder = "./Data/".$_ordnerpfad."/Rapport/";
+		if (!file_exists($_folder)){
+			mkdir($_folder, 0770);
+		}		
+		$fp = fopen($_file,"w+");
+		fputs($fp, $rapport);
+		fclose($fp);
 	}
 	function delete_rapport($_ordnerpfad, $_timestamp){
-        $_file = "./Data/".$_ordnerpfad."/Rapport/".date("Y.m.d", $_timestamp);
-        if (file_exists($_file)) {
-                unlink($_file);
-        }
+		$_file = "./Data/".$_ordnerpfad."/Rapport/".date("Y.m.d", $_timestamp);
+		if(file_exists($_file)){
+			unlink($_file);
+		}
 	}
 	function get_rapport($_ordnerpfad, $_timestamp){
-        $_file = "./Data/".$_ordnerpfad."/Rapport/".date("Y.m.d", $_timestamp);
-        if (!file_exists($_file)) {
-                $_txt = "Rapport für den " . date("d.m.Y",$_timestamp). "\n-";
-                                $_txt = "";
-        }else{
-                //$_txt = file($_file);
-                $_txt = file_get_contents($_file);
-        }
-        return $_txt;
+		$_file = "./Data/".$_ordnerpfad."/Rapport/".date("Y.m.d", $_timestamp);
+		if(!file_exists($_file)){
+			$_txt = "Rapport für den " . date("d.m.Y",$_timestamp). "\n-";
+			$_txt = "";
+		}else{
+			//$_txt = file($_file);
+			$_txt = file_get_contents($_file);
+		}
+		return $_txt;
 	}
 	
 }
